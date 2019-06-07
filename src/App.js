cd98478dabfca37bbdedfd2b1fbd8cc6952b3a05
John Livingston <john.livingston@pwc.com>
2019-06-07 14:00:18 -0700

    Update to version 0.0.21

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import BlueButton from 'jl-react-components-library/cjs/ui/BlueButton'
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import { BlueButton, GreenButton } from 'jl-react-components-library'
+import logo from './logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <BlueButton text="Test" onClick={console.log} />
-      <GreenButton text="Test" onClick={console.log} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <BlueButton text='Test' onClick={console.log} />
+      {/* <GreenButton text="Test" onClick={console.log} /> */}
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
